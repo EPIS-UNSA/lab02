80cda169f1ae417e162cf8db703c6303b0be6354
Omar Cristhian Romero Chipana <oromero@unsa.edu.pe>
2022-05-12 22:43:14 -0500

    ejercicio 1 html javascript desarrollado

---
+++ index.js
@@ -0,0 +1,10 @@
+// ejercicio 1
+var button_get_day = document.getElementById("getDay");
+button_get_day.onclick = function (){
+    var value = document.getElementById("day").value;	
+    var days = ['domingo','lunes','martes','miercoles','jueves','viernes','sabado'];
+    var today = new Date();
+    var result = document.getElementById("result_day");
+    result.textContent =days[value];
+    result.style.color = 'red';
+};
