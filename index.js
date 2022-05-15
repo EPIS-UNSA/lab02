e3863e283231307bd3eb7da2b8883c9989312d85
Omar Cristhian Romero Chipana <oromero@unsa.edu.pe>
2022-05-15 15:57:39 -0500

    usando new date

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-var dia = prompt("ingrese el dia del 1 al 6");
+/*var dia = prompt("ingrese el dia del 1 al 6");
 var arrayDias = ["domingo", "lunes", "martes", "miercoles", "jueves", "viernes", "sabado"];
 for(var i = 0; i<7; i++){
-    //document.write(arrayDias + "<br>");
     if(dia == i){
         alert("el dia es : " + arrayDias[i]);
         }
 }
+*/
+var dia = new Date();
+document.write(dia);
