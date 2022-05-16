6ab5f6f9ae32f958468d96d9ec0ee98c715b4d3f
Omar Cristhian Romero Chipana <oromero@unsa.edu.pe>
2022-05-15 22:15:44 -0500

    iniciando el ejercicio 2

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// introduciendonos en el ejercicio 1
 /*var dia = prompt("ingrese el dia del 1 al 6");
 var arrayDias = ["domingo", "lunes", "martes", "miercoles", "jueves", "viernes", "sabado"];
 for(var i = 0; i<7; i++){
@@ -6,13 +7,19 @@ for(var i = 0; i<7; i++){
         }
 }
 */
-var dia = new Date();
+
+// EJERCICIO NRO 1
+/*var dia = new Date();
 document.write(dia);
 var valor = dia.getDay();
 document.write(valor);
 var arrayDias = ["domingo", "lunes", "martes", "miercoles", "jueves", "viernes", "sabado"];
 for(var i = 0; i<7; i++){
     if(valor == i){
-        alert("el dia es : " + arrayDias[i]);
+        alert("Hoy dia es : " + arrayDias[i]);
         }
     }
+*/
+
+// EJERCICIO NRO 2.
+var texto = prompt("ingrese una palabra");
