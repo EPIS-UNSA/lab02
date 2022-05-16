b49c88d93720ff2ade3ccf4ced8a3932ab7046ef
Omar Cristhian Romero Chipana <oromero@unsa.edu.pe>
2022-05-15 22:05:33 -0500

    parece que ya funciona

--- index.js
+++ index.js
@@ -10,6 +10,9 @@ var dia = new Date();
 document.write(dia);
 var valor = dia.getDay();
 document.write(valor);
-var doa = 'el gato de mama';
-var cantidad = doa.slice(3, -2);
-document.write(cantidad);
+var arrayDias = ["domingo", "lunes", "martes", "miercoles", "jueves", "viernes", "sabado"];
+for(var i = 0; i<7; i++){
+    if(valor == i){
+        alert("el dia es : " + arrayDias[i]);
+        }
+    }
